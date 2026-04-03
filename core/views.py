8d93e81b62a6da8b3821b78c00a051bff62399ab
omri2006 <omri2927@gmail.com>
2026-04-03 17:41:04 +0300

    finished part 5 - added basic templates with basic dashboard, which is used to create simple home page with a link to the Django Admin Panel Page

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home_view(request):
+    context = {
+        'title': 'InsightForge',
+        'status': 'Dashboard Placeholder active'
+    }
+    return render(request, 'core/home.html', context)
