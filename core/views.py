b85617a26328eb40df40a540c0684a972644ad4e
omri2006 <omri2927@gmail.com>
2026-04-04 09:15:22 +0300

    completed part 6 - added new html file called dashboard.html that for now shows basic data about some things in the Database + for the first time getting real data (for now it is still not so real data) from the DB

--- core/views.py
+++ core/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from documents.models import Document
+from search_engine.models import SearchQuery
+from automation.models import AutomationJob, SystemLog
 
 # Create your views here.
 def home_view(request):
@@ -7,3 +10,15 @@ def home_view(request):
         'status': 'Dashboard Placeholder active'
     }
     return render(request, 'core/home.html', context)
+
+def dashboard_view(request):
+    context = {
+        'title': 'Dashboard',
+        'documents': Document.objects.filter(owner=request.user).count(),
+        'searches': SearchQuery.objects.filter(user=request.user).count(),
+        'jobs': AutomationJob.objects.filter(owner=request.user).count(),
+        'logs': SystemLog.objects.count(),
+        'recent_documents': Document.objects.order_by('-updated_at')[:5]
+    }
+
+    return render(request, 'core/dashboard.html', context)
